4f50b0627b26be4938c85604f831038314ed16ff
Chen <choak@qq.com>
2019-04-11 16:05:42 +0800

    20190411

--- PCBDetect/TemplateExtractor.cpp
+++ PCBDetect/TemplateExtractor.cpp
@@ -54,19 +54,42 @@ void TemplateExtractor::extract()
 	string templ_path = curr_path + detectParams->sampleModelNum.toStdString();//检查模板文件夹中mask文件是否存在
 	if (0 != _access(templ_path.c_str(), 0))
 		_mkdir(templ_path.c_str());
-	string	mask_path = templ_path + "/mask";
+	string	mask_path = templ_path + "/mask";//掩膜路径
 	if (0 != _access(mask_path.c_str(), 0))
 		_mkdir(mask_path.c_str());
+	string templbw_path = templ_path + "/bw";//模板二值图路径
+	if (0 != _access(templbw_path.c_str(), 0))
+		_mkdir(templbw_path.c_str());
+	string templbin_path = templ_path + "/bin";//模板特征路径
+	if (0 != _access(templbin_path.c_str(), 0))
+		_mkdir(templbin_path.c_str());
+	string subtempl_path = templ_path + "/subtempl";//模板路径
+	if (0 != _access(subtempl_path.c_str(), 0))
+		_mkdir(subtempl_path.c_str());
 
 	for (int col = 0; col < detectParams->nCamera; col++) {
-		string temp = templ_path + "/" + std::to_string(detectParams->currentRow_extract + 1) + "_" 
-			+ std::to_string(col + 1)  + detectConfig->ImageFormat.toUpper().toStdString();
 		Mat src = *((*cvmatSamples)[detectParams->currentRow_extract][col]);
-		cv::imwrite(temp, src);//保存模板图片
-		Mat mask = templFunc->find1(col, src);
 
+		//保存模版图片
+		cv::imwrite(subtempl_path + "/" + std::to_string(detectParams->currentRow_extract + 1) + "_" + std::to_string(col + 1) + detectConfig->ImageFormat.toStdString(), src);//保存模板图片
+		
+		//保存掩膜图片
+		Mat mask = templFunc->find1(col, src);
 		cv::imwrite(mask_path + "/" + std::to_string(detectParams->currentRow_extract + 1) + "_" 
 			+ std::to_string(col + 1) + "_mask" + detectConfig->ImageFormat.toStdString(), mask);
+
+		//保存二值化图片
+		Mat srcGray, templbw;
+		cv::cvtColor(src, srcGray, cv::COLOR_RGB2GRAY);
+		cv::adaptiveThreshold(srcGray, templbw, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY_INV, 2001, 0);
+		cv::Mat element_b = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+		cv::morphologyEx(templbw, templbw, cv::MORPH_OPEN, element_b);
+		cv::morphologyEx(templbw, templbw, cv::MORPH_CLOSE, element_b);
+		cv::imwrite(templbw_path + "/" + std::to_string(detectParams->currentRow_extract + 1) + "_" + std::to_string(col + 1) + "_bw" + detectConfig->ImageFormat.toStdString(), templbw);
+
+		//提取模板特征并保存
+		templFunc->save(templbin_path + "/" + std::to_string(detectParams->currentRow_extract + 1) + "_" + std::to_string(col + 1) + ".bin", src);
+
 		Rect roiRect = Rect(col*adminConfig->ImageSize_W, detectParams->currentRow_extract*adminConfig->ImageSize_H,
 			adminConfig->ImageSize_W, adminConfig->ImageSize_H);
 		Mat roi = templFunc->getBigTempl(roiRect);
