2956ce2a7f84e411c9f2decebe0907297c00a8b5
LiYang07 <38521399+LiYang07@users.noreply.github.com>
2019-07-01 15:14:32 +0800

    文件传输代码
    FileSender用于检测界面
    FileReceiver用于复查界面

---
+++ PCBDetect/FileSender.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <iostream>
+#include<string>
+#include<vector>
+#include <array>
+#include <fstream>
+#include<memory>
+#include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/log/trivial.hpp>
+
+class FileSender
+{
+public:
+	FileSender(std::string _ip,std::string port = "8888");//IPv4地址初始化，监听端口默认为8888
+	void SendFolder(const std::string& dir);//传入需要发送文件的完整路径
+	~FileSender();
+
+private:
+	std::string address;
+	std::string port ;
+private:
+	int getFilesName(const std::string& dir, std::vector<std::string>& filenames);//获取文件夹所有文件名
+};
+
+
+class Client
+{
+public:
+	using IoService = boost::asio::io_service;
+	using TcpResolver = boost::asio::ip::tcp::resolver;
+	using TcpResolverIterator = TcpResolver::iterator;
+	using TcpSocket = boost::asio::ip::tcp::socket;
+
+	Client(IoService& t_ioService, TcpResolverIterator t_endpointIterator,
+		std::string const& t_path);
+
+private:
+	void openFile(std::string const& t_path);
+	void doConnect();
+	void doWriteFile(const boost::system::error_code& t_ec);
+	template<class Buffer>
+	void writeBuffer(Buffer& t_buffer);
+
+
+	TcpResolver m_ioService;
+	TcpSocket m_socket;
+	TcpResolverIterator m_endpointIterator;
+	enum { MessageSize = 1024 * 40 };
+	std::array<char, MessageSize> m_buf;
+	boost::asio::streambuf m_request;
+	std::ifstream m_sourceFile;
+	std::string m_path;
+};
+
+
